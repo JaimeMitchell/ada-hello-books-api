bef381892b160f75710ec68cfd68dab497129878
Jaime Mitchell <JaimeMitchell100@gmail.com>
2022-10-20 22:07:21 -0400

    did my first routes

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,12 @@
 from flask import Flask
 
-def create_app(test_config=None):
+
+def create_app(test_config=None): #WHAT IS TEST_CONFIG = NONE
     app = Flask(__name__)
 
+    from.routes import hello_world_bp
+    app.register_blueprint(hello_world_bp)
+
     return app
+
+
