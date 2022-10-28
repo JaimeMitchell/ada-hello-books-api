b492210f9af1f4b7eddaa47c18f001d68186fd1e
Jaime Mitchell <JaimeMitchell100@gmail.com>
2022-10-27 21:41:50 -0400

    finished branch 03b-create

--- app/routes.py
+++ app/routes.py
@@ -1,50 +1,17 @@
-from flask import Blueprint, jsonify, abort, make_response
-
-# class Book:
-#     def __init__(self, id, title, description):
-#         self.id = id
-#         self.title = title
-#         self.description = description
-
-
-# books = [
-#     Book(1, "kite making", "a books about string management"),
-#     Book(2, "picture of dorian gray",
-#          "victorian lgbt social commentary, youth fixation"),
-#     Book(3, "coffee", "i need some right now")
-# ]
+from app import db
+from app.models.book import Book
+from flask import Blueprint, jsonify, make_response, request
 
 books_bp = Blueprint("books", __name__, url_prefix="/books")
 
 
-# @books_bp.route("", methods=["GET"])
-# def handle_all_books():
-#     books_response = []
-#     for book in books:
-#         books_response.append({
-#             "id": book.id,
-#             "title": book.title,
-#             "description": book.description
-#         })
-#     return jsonify(books_response), 200
+@books_bp.route("", methods=["POST"])
+def handle_books():
+    request_body = request.get_json()
+    new_book = Book(title=request_body["title"],
+                    description=request_body["description"])
 
+    db.session.add(new_book)
+    db.session.commit()
 
-# def validate(book_id):
-#     try:
-#         book_id = int(book_id)
-#     except:
-#         abort(make_response({"message": f"book {book_id} is invalid"}, 400))
-#     for book in books:
-#         if book.id == book_id:
-#             return(book)
-#     abort(make_response({"message": f"book {book_id} is non existant"}, 404))
-
-
-# @books_bp.route("/<book_id>", methods=['GET'])
-# def handle_book(book_id):
-#     book = validate(book_id)
-#     return{
-#         "id": book.id,
-#         "title": book.title,
-#         "description": book.description
-#     }
+    return make_response(f"Book {new_book.title} successfully created", 201)
