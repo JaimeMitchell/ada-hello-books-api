03a2ed0c22ac38b9bd1ce21d65116f3a7c5ed2ff
Jaime Mitchell <JaimeMitchell100@gmail.com>
2022-10-27 22:08:27 -0400

    finished get/post now time to refactor

--- app/routes.py
+++ app/routes.py
@@ -19,4 +19,15 @@ def handle_books():
         return jsonify(books_response)
     elif request.method == "POST":
         # ... Indent all of the Create Book functionality into this elif
-        # request_body = request.get_json()
+        request_body = request.get_json()
+        books_response = []
+        books = Book.query.all()
+        for book in books:
+            books_response.append(
+                {
+                    "id": book.id,
+                    "title": book.title,
+                    "description": book.description
+                }
+            )
+        return jsonify(books_response)
